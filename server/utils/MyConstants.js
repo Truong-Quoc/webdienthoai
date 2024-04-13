df19427207d37e8e712e4b684b4e612a6fb9f844
Truong-Quoc <128444721+Truong-Quoc@users.noreply.github.com>
2024-04-13 13:55:08 +0700

    Dự án

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,10 @@
+const MyConstants = {
+    DB_SERVER: "cluster0.apxtfgs.mongodb.net",
+    DB_USER: "nguyentruongquoc23032019",
+    DB_PASS: "rhJ5cbILApyEFb4V",
+    DB_DATABASE: "Cluster0",
+    EMAIL_USER: "<email_user>",
+    JWT_SECRET: "<jwt_secret>",
+    JWT_EXPIRES: "<999999999999999999999>",
+  };
+module.exports = MyConstants; 
